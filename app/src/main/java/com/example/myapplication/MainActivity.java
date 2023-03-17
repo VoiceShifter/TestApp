292cc504d3ac84035fc5dc08d0865eaf2bcbe6eb
unknown <gachi.muchi.98@mail.ru>
2023-03-17 23:20:31 +0300

    first part, layouts

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //showing the activity_main.xml
+        setContentView(R.layout.test_layout); //showing the activity_main.xml
 
         Toast toast = Toast.makeText(getApplicationContext() , "onCreat called" , Toast.LENGTH_LONG);
         toast.show();
